f3f9de0ac1c7c32273736e13c92730a3b8c1c502
Balaganesh <balaganesh2204@gmail.com>
2026-08-26 14:06:55 +0530

    Delete run_app.bat

--- run_app.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Convenience launcher for Bala Support.
-REM Uses "python -m streamlit" because this venv resolves packages
-REM through a .pth link and has no console-script shims.
-"%~dp0.venv\Scripts\python.exe" -m streamlit run "%~dp0app.py"
